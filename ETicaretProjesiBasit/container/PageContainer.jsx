dcaad5ad65b054c62a5c95441c0f47e43b3a802c
gulsengurbuz22 <125699735+GulsenGRBZ@users.noreply.github.com>
2025-10-11 04:57:57 +0300

    ETicaretProjesiBasit Kurs Projesi Eklendi

---
+++ ETicaretProjesiBasit/container/PageContainer.jsx
@@ -0,0 +1,7 @@
+import React, { Children } from "react";
+import Container from "@mui/material/Container";
+function PageContainer({ children }) {
+  return <Container>{children}</Container>;
+}
+
+export default PageContainer;
